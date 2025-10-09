8efd888be0f7f87717630d894250d2d1c2ef62b9
musicardi <juanmanuelcalabia@gmail.com>
2025-10-09 11:33:23 -0300

    Update train_model_dag.py
    Se migró de BashOperator a PythonOperator para ejecutar directamente la función de entrenamiento. Esto integra mejor la lógica con Airflow, facilita la reutilización de resultados y la gestión de errores, manteniendo la posibilidad de ejecutar el script de forma independiente si se desea.

--- dags/train_model_dag.py
+++ dags/train_model_dag.py
@@ -1,3 +1,4 @@
+'''
 from __future__ import annotations
 import pendulum
 
@@ -17,4 +18,28 @@ with DAG(
     train_model = BashOperator(
         task_id="ejecutar_entrenamiento",
         bash_command="python /opt/airflow/scripts/train_model.py"
+
+    )
+'''
+
+from __future__ import annotations
+import pendulum
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
+
+# Importar tu función de entrenamiento
+# Asegurate que train_model.py esté en el PYTHONPATH o mismo directorio
+from scripts.train_model import main as train_model_func
+
+with DAG(
+    dag_id="entrenamiento_modelo_ml",
+    start_date=pendulum.datetime(2025, 9, 16, tz="UTC"),
+    schedule=None,
+    catchup=False,
+    tags=["mlops", "mlflow", "train"],
+) as dag:
+
+    ejecutar_entrenamiento = PythonOperator(
+        task_id="ejecutar_entrenamiento",
+        python_callable=train_model_func
     )
